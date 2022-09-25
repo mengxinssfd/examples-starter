6700d7c62c81061e9b715748624cbcbba0beb4a8
dyh_a <xinzon32177@163.com>
2022-09-25 10:36:30 +0800

    feat: vue 单文件页面模版

---
+++ pages/page-2/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">hello world</div>
+</template>
+
+<script setup lang="ts">
+import { onMounted } from 'vue';
+
+onMounted(() => {
+  alert('hello world');
+});
+</script>
+<style lang="scss">
+.home {
+  font-size: 20px;
+}
+</style>
